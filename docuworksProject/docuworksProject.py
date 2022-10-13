1e316d6e6a9cf36c94976987aaf7e03528749feb
Peter van den Hemel <petervdhemel@gmail.com>
2022-10-13 16:05:12 +0200

    Initial search and replace functionality.

--- docuworksProject/docuworksProject.py
+++ docuworksProject/docuworksProject.py
@@ -28,6 +28,8 @@ def main():
         display(text)
     elif option == '2':
         search(text)
+    elif option == '3':
+        replace(text)
     elif option == '8':
         sys.exit()
     else:
@@ -36,8 +38,8 @@ def main():
     main()
     
 
-def display(text):
-    click.echo(text)
+def display(txt):
+    click.echo(txt)
     
 
 def search(txt):
@@ -53,6 +55,33 @@ def search(txt):
             click.echo(indices)
         else:
             click.echo("No results found.")
+        
+        click.echo("\nWould you like to try again? y/n\n")
+        option = click.getchar()
+
+def replace(txt):
+    '''Searches through the text using a user input string and replaces text'''
+    option = 'y'
+    while option == 'y':
+        # Initalize new text object for replaced text
+        newtxt = 'replaced'
+
+        search = input("Please provide an input string for searching: ")
+        replace = input("Please provide an input string for replacing: \n")
+
+        txt = re.sub(search, replace, txt)
+        click.echo(f"New text: \n{txt}")
+
+        click.echo("Do you want to save the edited file as a new file? y/n: ")
+        option = click.getchar()
+
+        if option == 'y':
+            name = input("Please enter the file name: ")
+            name = name + ".txt"
+            with open(name, w) as newFile:
+                newFile.seek(0)
+                newFile.write(txt)
+                newFile.truncate()
         
         click.echo("\nWould you like to try again? y/n\n")
         option = click.getchar()
