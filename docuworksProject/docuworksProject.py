678179a03b8f9eb07e605a49d6ecc0ce25f1da40
Peter van den Hemel <petervdhemel@gmail.com>
2022-10-14 22:00:41 +0200

    Find most common phrases implemented.

--- docuworksProject/docuworksProject.py
+++ docuworksProject/docuworksProject.py
@@ -1,3 +1,4 @@
+from json import load
 from tkinter import Toplevel
 import click
 import sys
@@ -27,6 +28,10 @@ class TextProcessor(ABC):
     def save(self, path: Path) -> None:
         raise NotImplementedError
 
+    @abstractmethod 
+    def findCommon(self, limit) -> None:
+        raise NotImplementedError
+
 class MyTextProcessor(TextProcessor):
     def load(self, path):
         with click.open_file(path, "r") as file:
@@ -36,26 +41,42 @@ class MyTextProcessor(TextProcessor):
         click.echo(self.text)
 
     def iterSearch(self, searchPhrase):
-        result = re.finditer(searchPhrase, self.text)
+        result = re.finditer(searchPhrase, self.text) # Iteratively searches phrase using regex
 
         if result != None:
             click.echo("Index of found searched phrase:")
-            indices = [index.start() for index in result]
+            indices = [index.start() for index in result] # Creates a list of indices for each index in result
             click.echo(indices)
         else:
             click.echo("No results found.")
     
     def replace(self, searchStr, replaceStr):
+        # Initalize new text object for replaced text
         self.newTxt = re.sub(searchStr, replaceStr, self.text)
 
         click.echo(f"New text: \n{self.newTxt}")
 
     def save(self, path):
         with click.open_file(path, 'w') as newFile:
-                newFile.seek(0)
+                newFile.seek(0) # Start at beginning of the file.
                 newFile.write(self.newTxt)
                 newFile.truncate()
 
+    def findCommon(self, limit):
+            '''Finds the most common words in text. 'limit' Is the amount of common words shown.'''
+            words = self.text.split(" ")
+            words_count = Counter(words).most_common()
+            for x in range(limit):
+                click.echo(f"Most frequent word place {x + 1} is: {words_count[x][0]} with {words_count[x][1]} occurrences.")
+
+            click.pause()
+
+def loadApp():
+    '''Calls text processor class and loads the class. Returns class.'''
+    app = MyTextProcessor()
+    app.load(Path(r"text.txt"))
+
+    return app
 
 @click.command()
 def main():
@@ -92,8 +113,8 @@ def main():
     
 @click.command()
 def display():
-    app = MyTextProcessor()
-    app.load(Path(r"text.txt"))
+    app = loadApp()
+
     app.display()
     click.pause()
     
@@ -105,8 +126,7 @@ def search():
         click.clear()
         search = input("Please provide an input string for searching: ")
 
-        app = MyTextProcessor()
-        app.load(Path(r"text.txt"))
+        app = loadApp()
         app.iterSearch(search)
 
        
@@ -118,15 +138,12 @@ def replace():
     '''Searches through the text using a user input string and replaces text'''
     option = 'y'
     while option == 'y':
-        # Initalize new text object for replaced text
-        newTxt = 'replaced'
         click.clear()
 
         search = input("Please provide an input string for searching: ")
         replace = input("Please provide an input string for replacing: ")
 
-        app = MyTextProcessor()
-        app.load(Path(r"text.txt"))
+        app = loadApp()
         app.replace(search, replace)
 
         click.echo("\nDo you want to save the edited file as a new file? y/n: ")
@@ -147,20 +164,22 @@ def commonWords():
     '''Finds the most commonly used words in the text.'''
     option = 'y'
     while option == 'y':
+        click.clear()
+        app = loadApp()
+
         toplimit = input("How many of the most used words should be shown?: ")
+
         try:
-            int(toplimit)
+            toplimit = int(toplimit)
         except:
             click.echo("Please enter a valid number.")
-        else:
-            words = txt.split(" ")
-            words_count = Counter(words).most_common()
-            click.echo(f"Most frequent word used is: {words_count[0][0]} with {words_count[0][1]} occurrences.")
             click.pause()
+        else:
+            app.findCommon(toplimit)
 
-            click.clear()
-            click.echo("Would you like to try again? y/n\n")
-            option = click.getchar()
+        click.clear()
+        click.echo("Would you like to try again? y/n\n")
+        option = click.getchar()
 
 
 if __name__ == "__main__":
